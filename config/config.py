f775889a44ed36d6ead13c2a353c8e3c1f80828c
Natalya Segal <natalya.segal@gmail.com>
2024-04-28 17:09:46 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import LabelBinarizer
 from typing import Any, List
+import yaml
+from split import Logical_Split
 
 def binarize_lables(labels_orig: List[Any]) -> List[int]:
       """
@@ -14,10 +16,18 @@ def binarize_lables(labels_orig: List[Any]) -> List[int]:
       lb = LabelBinarizer()
       return lb.fit_transform(labels_orig)
 
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
 class Configuration():
   def __init__(self, verbose = True):
-    self.subjects_and_dates_config_file_name = 'config/subjects_and_dates.yml'
+    self.verbose = verbose  
+    self.subjects_and_dates_config_file_name = 'config/subjects_and_dates.yaml'
     self.models_path='models'
+    self.be_consistent = True # initializze seeds for consistent results
+    self.seed_for_init = 1 #seed for initialization
     self.raw_data_path = 'exp3'
     self.data_path = 'data' #destination after preprocessing
     self.MAX_CHUNKS_PER_CATEGORY = 100000
@@ -33,5 +43,32 @@ class Configuration():
     self.number_of_classes = len(CLASSES)
     self.lables_categories = [x for x in range(self.number_of_classes)]
     self.binary_lables = binarize_lables(self.lables_categories)
-    if verbose:
+    if self.verbose:
       print(f' number_of_classes = {self.number_of_classes}\n binary_lables={self.binary_lables}')
+
+class Configuration_Gen(Configuration):
+  def __init__(self, verbose = True):
+    super().__init__(verbose)
+    self.sample_splits_file_name = 'config/sample_gen_split.yaml'
+    self.split_num = 6
+    self.sample_splits = load_yaml(self.sample_splits_file_name)
+    print(self.sample_splits)
+    train_mix, val_mix, test_mix, model_name = self.sample_splits[self.split_num].values()
+    print(f'{train_mix}, {val_mix}, {test_mix}, {model_name}')
+    self.set_split_by_subjects(train_mix, val_mix, test_mix)
+
+  def set_split(self, train_dates, train_subjects, val_dates, val_subjects, test_dates, test_subjects):
+    self.train_dates = train_dates
+    self.train_subjects = train_subjects
+    self.val_dates = val_dates
+    self.val_subjects = val_subjects
+    self.test_dates = test_dates
+    self.test_subjects = test_subjects
+
+  def set_split_by_subjects(self, train_mix, val_mix, test_mix):
+    ls = Logical_Split(self.subjects_and_dates_config_file_name, verbose = self.verbose)
+    train_dates, train_subjects = ls.get_dates_and_subjects(train_mix, Logical_Split.Sample_time.MORNING_AND_MID_DAY)
+    val_dates, val_subjects = ls.get_dates_and_subjects(val_mix, Logical_Split.Sample_time.MORNING_AND_MID_DAY)
+    test_dates, test_subjects = ls.get_dates_and_subjects(test_mix, Logical_Split.Sample_time.MORNING_AND_MID_DAY)
+    print(f' train {train_dates} {train_subjects}\n val {val_dates} {val_subjects}\n test {test_dates} {test_subjects}')
+    self.set_split(train_dates, train_subjects, val_dates , val_subjects, test_dates, test_subjects)
