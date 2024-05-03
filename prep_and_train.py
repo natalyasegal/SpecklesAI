a4f503cbb219b3aa0f7d702fb5abf7e1e4f47e8c
Natalya Segal <natalya.segal@gmail.com>
2024-05-03 22:46:17 +0300

    Update prep_and_train.py

--- prep_and_train.py
+++ prep_and_train.py
@@ -3,55 +3,63 @@ from preprocessing import Preprocessing
 from evaluation.eval import evaluate_per_chunk
 from models.model_speech_convlstm_tf import train_model
 import numpy as np
+from visualization.visualization import visualize_speckles
 
-def save_dataset(x, y, file_name = 'train_set.npy'):
+
+def save_dataset_x(x, file_name):
+  with open(file_name, 'wb') as f:
+      np.save(f, x)
+
+def save_dataset(x, y, file_name):
   with open(file_name, 'wb') as f:
       np.save(f, x)
       np.save(f, y)
 
-def load_dataset(file_name = 'train_set.npy'):
+def load_dataset(file_name):
   with open(file_name, 'rb') as f:
     x = np.load(f)
     y = np.load(f)
   return x, y
-  
-def visualize_speckles(x_train, save_path = 'speckles_sample.png', please_also_show = False):
-  # Construct a figure on which we will visualize the images.
-  fig, axes = plt.subplots(4, 5, figsize=(9, 8))
 
-  # Plot each of the sequential images for one random data example.
-  data_choice = np.random.choice(range(len(x_train)), size=1)[0]
-  for idx, ax in enumerate(axes.flat):
-      ax.imshow(np.squeeze(x_train[data_choice][idx]), cmap="gray")
-      ax.set_title(f"Frame {idx + 1}")
-      ax.axis("off")
+def get_or_create_dataset(config, need_to_create = True, need_to_save = False):
+  if need_to_create:
+    prep = Preprocessing(config, verbose = config.verbose)
+    prep.create_data_set() #videos to frames for train, validation and test sets
+    x_train, y_train, x_val, y_val = prep.prepare_train_and_validation_data()
+    x_test, y_test, x_test_per_category = prep.prepare_test_data()
+    if need_to_save:
+      save_dataset(x_train, y_train, file_name = 'train_set.npy')
+      save_dataset(x_val, y_val, file_name = 'validation_set.npy')
+      save_dataset(x_test, y_test, file_name = 'test_set.npy')
+      save_dataset_x(x_test_per_category, file_name = 'test_per_category.npy')
+  else:
+    x_train, y_train = load_dataset(file_name = 'train_set.npy')
+    x_val, y_val = load_dataset(file_name = 'validation_set.npy')
+    x_test, y_test = load_dataset(file_name = 'test_set.npy')
+    x_test_per_category = load_dataset(file_name = 'test_per_category.npy')
+  if config.create_images:
+    visualize_speckles(x_train, save_path = 'speckles_sample.png', please_also_show = False)
+  return x_train, y_train, x_val, y_val, x_test, y_test, x_test_per_category
 
-  # Save the figure.
-  print(f"Displaying sample frames {data_choice}.")
-  plt.savefig(save_path, bbox_inches='tight')
-  #plt.show() #uncomment if used in notebook
-
-def preprocess_and_train(args_batch_sz, args_n_epochs, create_images = True):
+def preprocess_and_train(args_batch_sz, args_n_epochs):
   config = Configuration_Gen(verbose = True)
   if config.be_consistent:
     play_consistent(seed_for_init = config.seed_for_init)
-  prep = Preprocessing(config, verbose = config.verbose)
-  prep.create_data_set() #videos to frames for train, validation and test sets
-  x_train, y_train, x_val, y_val = prep.prepare_train_and_validation_data()
-  save_dataset(x_train, y_train, file_name = 'train_set.npy')
-  save_dataset(x_val, y_val, file_name = 'validation_set.npy')
-  x_test, y_test = prep.prepare_test_data()
-  save_dataset(x_test, y_test, file_name = 'test_set.npy')
-  if create_images:
-    visualize_speckles(x_train, save_path = 'speckles_sample.png', please_also_show = False)
+  x_train, y_train, x_val, y_val, x_test, y_test, x_test_per_category = get_or_create_dataset(
+      config, need_to_create = True, need_to_save = False) 
   model_ex3_10, model_history = train_model(config, 9, 8, 
                                             x_train, y_train, 
                                             x_val, y_val,                                        
                                             batch_sz = args_batch_sz, n_epochs = args_n_epochs)
   model = tf.keras.models.load_model(config.models_path)
-  res_df = evaluate_per_chunk(config, model, x_test, y_test, create_images = True)
-  return model, model_history, x_test, y_test, x_train, y_train, x_val, y_val, config, res_df
+  res_df = evaluate_per_chunk(config, model, x_test, y_test)
+  return model, model_history, x_test_per_category, x_test, y_test, x_train, y_train, x_val, y_val, config, res_df
 
-model, model_history, x_test, y_test, x_train, y_train, x_val, y_val, config, res_df = preprocess_and_train(
-    args_batch_sz=1000, args_n_epochs=50)
+#TODO: create main:
+
+model, model_history, x_test_per_category, x_test, y_test, x_train, y_train, x_val, y_val, config, res_df = preprocess_and_train(
+    args_batch_sz=1000, args_n_epochs=25)
+ref_df_a = eval_accumulated(config, model, x_test_per_category, num_of_chunks_to_aggregate = 25)
+
+
 
