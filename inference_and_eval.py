33d75de486ea93697db7ac816081d289902d7057
Natalya Segal <natalya.segal@gmail.com>
2024-12-26 13:21:30 +0200

    Update inference_and_eval.py
    Support ensemble model inference

--- inference_and_eval.py
+++ inference_and_eval.py
@@ -5,8 +5,8 @@ import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from config.config import Configuration, Configuration_Gen, Configuration_PerSubjExperiment     
 from preprocessing.preprocessing import Preprocessing
-from evaluation.eval import evaluate_per_chunk, eval_accumulated
-from models.model_speech_convlstm_tf import load_model, set_seed
+from evaluation.eval import evaluate_per_chunk, evaluate_per_chunk_e, eval_accumulated, eval_accumulated_e
+from models.model_speech_convlstm_tf import load_model, load_model_from_path, set_seed
 from visualization.visualization import visualize_speckles
 from utils.utils import save_dataset_x, load_dataset_x
 
@@ -30,6 +30,30 @@ def get_or_create_test_dataset(config, args, need_to_save):
     visualize_speckles(x_test, save_path = 'speckles_sample.png', please_also_show = False)
   return x_test, y_test, x_test_per_category
 
+def list_subdirectories(directory):
+    """
+    Lists and prints the names of subdirectories one level down in the given directory.
+    Args:
+        directory (str): The path to the directory to process.
+    Returns:
+        list: A list of names of the subdirectories one level down.
+    """
+    subdirectories = []
+    try:
+        # List the contents of the directory
+        for item in os.listdir(directory):
+            full_path = os.path.join(directory, item)
+            # Check if the item is a directory
+            if os.path.isdir(full_path):
+                print(item)
+                subdirectories.append(full_path)
+    except FileNotFoundError:
+        print(f"The directory '{directory}' was not found.")
+        assert(True)
+    except PermissionError:
+        print(f"Permission denied for accessing '{directory}'.")
+    return subdirectories
+
 
 def main(args):
   if args.use_per_subj_config:
@@ -40,14 +64,22 @@ def main(args):
   if config.be_consistent:
     set_seed(seed_for_init = config.seed_for_init, random_seed = args.random_seed)
   x_test, y_test, x_test_per_category = get_or_create_test_dataset(config, args, need_to_save = True) 
-  model = load_model(config) 
-     
+  
   print("x_test shape:", x_test.shape)
   print("y_test shape:", y_test.shape)
   print("x_test_per_category shape:", x_test_per_category.shape)
   
-  res_df = evaluate_per_chunk(config, model, x_test, y_test)
-  ref_df_a = eval_accumulated(config, model, x_test_per_category, num_of_chunks_to_aggregate = args.num_of_chunks_to_aggregate)
+  if not args.need_ensemble:
+    model = load_model(config)
+    res_df = evaluate_per_chunk(config, model, x_test, y_test)
+    ref_df_a = eval_accumulated(config, model, x_test_per_category, num_of_chunks_to_aggregate = args.num_of_chunks_to_aggregate)
+  else:
+    directory_path = "all_models"
+    subdirs = list_subdirectories(directory_path)
+    print("Subdirectories:", subdirs)
+    all_models = [load_model_from_path(model_path, True) for model_path in subdirs]
+    res_df, bin_predictions, num_models = evaluate_per_chunk_e(config, all_models, x_test, y_test)
+    eval_accumulated_e(config, all_models, x_test_per_category, num_of_chunks_to_aggregate = args.num_of_chunks_to_aggregate)
 
 
 if __name__ == '__main__':
@@ -76,7 +108,10 @@ if __name__ == '__main__':
     parser.add_argument('--use_per_subj_config', 
                         action='store_true',
                         help='If specified, uses per_subj experiment config option, this affects mostly printouts. Use pre-created datasets with this option.')
+                        
+    parser.add_argument('--need_ensemble', 
+                        action='store_true',
+                        help='If specified, uses ensemple of models, models should reside in their subdirectories in all_models directory.')
 
-   
     args = parser.parse_args()
     main(args)
