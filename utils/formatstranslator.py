905ab3ee0f43b49132a5c538bb191fdb94f73c48
Natalya Segal <natalya.segal@gmail.com>
2025-11-22 22:45:59 +0200

    Implement data rearrangement
    Added functions for label binarization and input data rearrangement.

---
+++ utils/formatstranslator.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+
+def binarize_lables(labels_orig):
+      lb = LabelBinarizer()
+      return lb.fit_transform(labels_orig)
+
+def rearrange_input_s(x, need_to_shuffle_within_category, 
+                      MAX_CHUNKS_PER_CATEGORY, number_of_classes = 2):
+      '''
+      Rearranges the input data by randomly shuffling each category and then limits
+      the data size to the configured maximum chunks per category.
+      '''
+      binary_lables = binarize_lables([x for x in range(number_of_classes)])
+      y = [[] for _ in range(number_of_classes)]
+      print(f'max_chanks={MAX_CHUNKS_PER_CATEGORY}')
+      for i in range(number_of_classes): #short loop, as number of categories
+          if len(x[i]) == 0:
+            print(f'no items in position {i}')
+            continue
+          print(f' x[{i}] shape is {np.shape(x[i])}')
+          if need_to_shuffle_within_category:
+            p = np.random.permutation(np.shape(x[i])[0])
+            print(' Shaffling within the categories, permutation p shape is {np.shape(p)}')
+            x[i] = np.array(x[i])
+            x[i] = x[i][p, ::]
+          real_max = min(np.shape(x[i])[0], MAX_CHUNKS_PER_CATEGORY)
+          x[i] = x[i][0:real_max, ::]
+          y[i] = np.full((np.shape(x[i])[0], len(binary_lables[0])), binary_lables[i])
+      return x, np.array(y)
+
+def limit_rearrange_and_flatten_s(input_data, need_to_shuffle_within_category, MAX_CHUNKS_PER_CATEGORY):
+    # Rearrange the input data and get the corresponding labels
+    rearranged_data, labels = rearrange_input_s(input_data, need_to_shuffle_within_category, MAX_CHUNKS_PER_CATEGORY)
+
+    # Concatenate the rearranged data and labels along the first axis
+    output_data = np.concatenate(rearranged_data, axis=0)
+    output_labels = np.concatenate(labels, axis=0)
+
+    # Return the concatenated and rearranged data along with their labels
+    return output_data, output_labels
+
+def test2trainformat(input_data, need_to_shuffle_within_category = False, MAX_CHUNKS_PER_CATEGORY = 500000):
+    x, y = limit_rearrange_and_flatten_s(input_data, need_to_shuffle_within_category)
+    return x, y #subject_normalize_preserve_time(x), y
