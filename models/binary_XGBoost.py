78f33ca0d8723d47017e30b517a4d8fcd319d5c2
Natalya Segal <natalya.segal@gmail.com>
2026-02-27 14:52:26 +0200

    Import concat_temporal_embeddings and update sys.path
    Add path to current directory and import utility function.

--- models/binary_XGBoost.py
+++ models/binary_XGBoost.py
@@ -6,6 +6,9 @@ from sklearn.metrics import (classification_report, confusion_matrix, ConfusionM
     roc_auc_score, accuracy_score, roc_curve)
 from xgboost.callback import EarlyStopping
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from utils.embeddings_utils import concat_temporal_embeddings
+
 ''' AUC-ROC curve Presentation '''
 def _closest_idx(thr, target):
       # choose index with minimal |thr - target|
