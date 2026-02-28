b7826fa796196275be4c936728c6c9bd44411db6
Natalya Segal <natalya.segal@gmail.com>
2026-02-28 17:03:47 +0200

    Add multiclass classifier training function

--- models/XGBoost_on_LV_MAE_embeddings.py
+++ models/XGBoost_on_LV_MAE_embeddings.py
@@ -16,7 +16,7 @@ from utils.data import split_from_start
 
 
 def train_and_eval_classifier_on_embeddings(test_inp, train_n=250, val_n=250, 
-                                            K = 1, class_names_list = ["MI", "CC"]):
+                                            K = 1, class_names_list = None):
 
   model, opt2, scaler2, start_ep = load_for_resume_and_infer(VideoMAE, "artifacts_lvmae_1/checkpoint.pt")
   X_train, X_val, X_test, y_train, y_val, y_test = split_from_start(test_inp, train_n=train_n, val_n=val_n)
@@ -29,3 +29,26 @@ def train_and_eval_classifier_on_embeddings(test_inp, train_n=250, val_n=250,
   return train_eval_xgboost_classifier(Z_train,y_train,Z_val,y_val,Z_test,y_test,
                                        K = K, class_names_list = class_names_list)
 
+
+def train_and_eval_multiclass_classifier_on_embeddings(inp_data, train_n=250, val_n=250,
+                                        K = 1, class_names_list = None):
+  model, opt2, scaler2, start_ep = load_for_resume_and_infer(VideoMAE, "artifacts_lvmae_1/checkpoint.pt")
+  X_train, X_val, X_test, y_train, y_val, y_test = split_from_start(inp_data,
+                                                                    train_n=train_n, val_n=val_n)
+  y_train = np.argmax(y_train, axis=1)
+  y_val   = np.argmax(y_val,   axis=1)
+  y_test  = np.argmax(y_test,  axis=1)
+
+  print("Train:", X_train.shape, y_train.shape)
+  print("Val:",   X_val.shape,   y_val.shape)
+  print("Test:",  X_test.shape,  y_test.shape)
+  
+  print("Train labels:", np.unique(y_train, return_counts=True))
+  print("Val labels:  ", np.unique(y_val,   return_counts=True))
+  print("Test labels: ", np.unique(y_test,  return_counts=True))
+
+  Z_train, y_train = extract_embeddings_wrapper_one(model, X_train, y_train)
+  Z_val, y_val  = extract_embeddings_wrapper_one(model, X_val, y_val)
+  Z_test, y_test = extract_embeddings_wrapper_one(model, X_test, y_test)
+
+  return train_eval_xgboost_classifier_multiclass(Z_train,y_train,Z_val,y_val,Z_test,y_test,K=K, class_names=class_names_list)
