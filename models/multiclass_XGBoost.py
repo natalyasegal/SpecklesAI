51ff7b857170d4f68c253583f5daaefc3aa786c7
Natalya Segal <natalya.segal@gmail.com>
2026-02-28 14:50:06 +0200

    Import numpy in multiclass_XGBoost.py
    Added numpy import for numerical operations.

--- models/multiclass_XGBoost.py
+++ models/multiclass_XGBoost.py
@@ -2,6 +2,7 @@ from sklearn.preprocessing import label_binarize
 from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
 import xgboost as xgb 
+import numpy as np
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_auc_score
 
 import sys
