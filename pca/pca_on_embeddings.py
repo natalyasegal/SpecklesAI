363267ae6f0bc3c1fda49e7508aadadcd46a7716
Natalya Segal <natalya.segal@gmail.com>
2026-04-09 16:11:22 +0300

    Implement 3D PCA visualization for embeddings
    Added a new function to create and visualize 3D embeddings using PCA.

---
+++ pca/pca_on_embeddings.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('/content/SpecklesAI')   # add package root to Python path
+
+from models.LvMAE_pt import extract_embeddings_wrapper_one
+from utils.embeddings_utils import concat_temporal_embeddings # sliding window -> target size: N-k+1
+from pca.pca import visualize_embeddings_pca_3d
+
+
+def create_and_visualize_embeddings_3d(x, y, class_names=["class0", "class1"], K=1):
+  model, opt2, scaler2, start_ep = load_for_resume_and_infer(VideoMAE, "artifacts_lvmae_1/checkpoint.pt")
+  x, y = extract_embeddings_wrapper_one(model, x, y)
+  x, y = concat_temporal_embeddings(x, y, K)
+  return visualize_embeddings_pca_3d(X=x, y=y, class_names=class_names)
+  
+def create_and_visualize_embeddings_multiclass_3d(x, y, class_names=["class0", "class1"], K=1):
+  model, opt2, scaler2, start_ep = load_for_resume_and_infer(VideoMAE, "artifacts_lvmae_1/checkpoint.pt")
+  y = np.argmax(y, axis=1)
+  x, y = extract_embeddings_wrapper_one(model, x, y)
+  x, y = concat_temporal_embeddings(x, y, K)
+  return visualize_embeddings_pca_3d(X=x, y=y, class_names=class_names)
