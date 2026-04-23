3b855de234ce2bc35a8c9f4826f6a94416694a62
Natalya Segal <natalya.segal@gmail.com>
2026-04-23 15:05:11 +0300

    Update eval_on_embeddings.py

--- evaluation/eval_on_embeddings.py
+++ evaluation/eval_on_embeddings.py
@@ -1,9 +1,4 @@
-
-
-def TestGen_ValHeldoutFromUnseen(
-          train_x_list = [x1_n, x2_n, x3_n, x4_n, x5_n, x6_n, x7_n, x9_n, x10_n], 
-          train_y_list = [y1_n, y2_n, y3_n, y4_n, y5_n, y6_n, y7_n, y9_n, y10_n],
-          unseen = test_8_m_n_40ms, K_thr = 500, num_of_chunks_to_aggregate = 25):
+def TestGen_ValHeldoutFromUnseen(train_x_list, train_y_list, unseen = test_8_m_n_40ms, K_thr = 500, num_of_chunks_to_aggregate = 25):
   model, opt2, scaler2, start_ep = load_for_resume_and_infer(VideoMAE, "artifacts_lvmae_1/checkpoint.pt")
   X_train, y_train =  concatenate_train_or_val(train_x_list, train_y_list)
   val, test = split_by_chunks_v(unseen, val_n = K_thr)
