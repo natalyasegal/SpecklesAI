a3a5f67f7ea40e93ea4c26eb73ce295428dc7bff
Natalya Segal <natalya.segal@gmail.com>
2026-07-29 21:25:04 +0300

    Fix return statement in XGBoost training function

--- models/XGBoost_on_LV_MAE_embeddings.py
+++ models/XGBoost_on_LV_MAE_embeddings.py
@@ -99,9 +99,9 @@ def TestGen_ValHeldoutFromUnseen_multiclass(train_x_list, train_y_list, unseen, 
   Z_val, y_val,  = extract_embeddings_wrapper_one(model, X_val, y_val)
   Z_test, y_test = extract_embeddings_wrapper_one(model, X_test, y_test)
 
-  clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypv,ypt,test_macro_f1,cm=train_eval_xgboost_classifier_multiclass_opt_th(Z_train,y_train,
+  clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypt,test_macro_f1,cm=train_eval_xgboost_classifier_multiclass_opt_th(Z_train,y_train,
                                               Z_val,y_val, Z_test,y_test, K = k, show=True, class_names=class_names) # 128w and 128
   #print(f'Aggregated k={num_of_chunks_to_aggregate}: =================================')
   #eval_aggregated_test_set_th_on_val(Z_test, prob_test, y_test, prob_val, y_val, num_of_chunks_to_aggregate= num_of_chunks_to_aggregate)
-  return clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypv,ypt,test_macro_f1,cm
+  return clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypt,test_macro_f1,cm
 
