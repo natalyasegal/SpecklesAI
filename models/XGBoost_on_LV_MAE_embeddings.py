420b3ea9e0e3e138b59610c57ab28f8339c04cf0
Natalya Segal <natalya.segal@gmail.com>
2026-07-30 15:14:20 +0300

    Add Z_test, y_test, y_val to return values

--- models/XGBoost_on_LV_MAE_embeddings.py
+++ models/XGBoost_on_LV_MAE_embeddings.py
@@ -103,5 +103,5 @@ def TestGen_ValHeldoutFromUnseen_multiclass(train_x_list, train_y_list, unseen, 
                                               Z_val,y_val, Z_test,y_test, K = k, show=True, class_names=class_names) # 128w and 128
   #print(f'Aggregated k={num_of_chunks_to_aggregate}: =================================')
   #eval_aggregated_test_set_th_on_val(Z_test, prob_test, y_test, prob_val, y_val, num_of_chunks_to_aggregate= num_of_chunks_to_aggregate)
-  return clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypt,ypv,test_macro_f1,cm
+  return clf,val_auc,test_auc,val_acc,test_acc,proba_val,proba_test,ypt,ypv,Z_test,y_test,y_val,test_macro_f1,cm
 
